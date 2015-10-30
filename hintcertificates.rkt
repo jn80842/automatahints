533a0b1115f57eca8de25e96b6252d303decd173
Julie Newcomb <jn80842@gmail.com>
2015-10-30 14:35:34 -0700

    update hint certificate

--- hintcertificates.rkt
+++ hintcertificates.rkt
@@ -3,10 +3,12 @@
 (require unstable/automata/dfa)
 
 ;;;;; helper functions ;;;;;
-(define (valid-word S T word)
+(define (good-outcome-word S T word)
   (eq? (machine-accepts? S word) (machine-accepts? T word)))
-(define (invalid-word S T word)
+(define (bad-outcome-word S T word)
   (not (eq? (machine-accepts? S word) (machine-accepts? T word))))
+;; add function 'consumed' / 'not-stuck'
+
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;            hints belong in NP                   ;;
@@ -15,26 +17,29 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 ;;;; validate counterexample ;;;;
+;assumption that L(S) and L(T) not equal
 (define (counterexample-checker word S T)
   (not (eq? (machine-accepts? S word) (machine-accepts? T word))))
 ; fitness: shorter word is better
 
 ;;;; minimal pairs ;;;;
-(define (minimalpairs-checker S T)
+(define (minimalpairs-checker S T )
   (and (eq? (length goodlist) (length badlist)) ; or, desired length of min pairs list
-       (and (andmap (lambda (w) (valid-word S T w) goodlist)
-            (andmap (lambda (w) (invalid-word S T w) badlist))))
+       (and (andmap (lambda (w) (good-outcome-word S T w) goodlist)
+            (andmap (lambda (w) (bad-outcome-word S T w) badlist))))
 ; fitness: shorter edit distance between pairs is better
 
 ;;;; strings arrive in wrong state ;;;;
 (define (wrongstate-checker S T)
-  (and (andmap (lambda (w) (invalid-word S T w)) cert-list)
+  (and (andmap (lambda (w) (good-outcome-word S T w)) cert-list)
        (andmap (lambda (s) (eq? s (final-state S (car cert-list)))) cert-list)))
+; fitness for this and next 2 hints: longer lists of cert words better
+; such lists are likely either finite and fairly short or arbitrarily long however
 
 ;;;; split states ;;;;
-(define (splitstate-checker S T)
-  (and (andmap (lambda (w) (valid-word S T w)) goodlist)
-       (and (andmap (lambda (w) (invalid-word S T w)) badlist)
+(define (splitstate-checker S T goodlist badlist)
+  (and (andmap (lambda (w) (good-outcome-word S T w)) goodlist)
+       (and (andmap (lambda (w) (bad-outcome-word S T w)) badlist)
             (andmap (lambda (s) (eq? s (final-state S (car goodlist)))) (append goodlist badlist)))))
 
 ;;;; prefix of some misclassified strings ;;;;
@@ -42,8 +47,6 @@
 ; prove that from this prefix many words don't arrive in good state
 (define (misclassifiedprefix-checker S T)
   (andmap (lambda (w) (invalid-word S T (append prefix w))) cert-list))
-; fitness for last 3 hints: longer lists of cert words better
-; such lists are likely either fairly short (epsilon) or arbitrarily long however
 
 ;;;; synthesized descriptions of states ;;;;
 ; certificate is list of mosel descriptions
