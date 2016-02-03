4da874bd02b4a9c3639381c6082a63442a50e61c
Julie Newcomb <newcombj@cs.washington.edu>
2016-02-02 23:18:37 -0800

    refactor rosette hints to use new interface

--- rosettehints.rkt
+++ rosettehints.rkt
@@ -63,19 +63,23 @@
                  (1 → s2)]))
 
 (printf "Prefix hint:\n")
-(define (prefixer w)
-  (define p (list (??) (??) (??)))
-         (append p w))
+(define prefix (exists-word-forall-words S2 T2 (list 0 1) 3 bad-prefix-pred))
+(if (empty? prefix)
+    (printf "No prefix of length ~a or less was found.\n\n" 3)
+    (printf "The prefix ~a st. p followed by any word up to length ~a will not have the desired behavior.\n\n" (word-value prefix) 3))
+;(define (prefixer w)
+;  (define p (list (??) (??) (??)))
+;         (append p w))
 
-(define (prefix-check m1 m2 prefixer w)
-  (assert (not (same-outcome? m1 m2 (prefixer w)))))
+;(define (prefix-check m1 m2 prefixer w)
+;  (assert (not (same-outcome? m1 m2 (prefixer w)))))
 
-(define ww (symbolic-word* 3 '(0 1)))
-(define binding
-  (synthesize #:forall (list ww)
-              #:guarantee (prefix-check S2 T2 prefixer ww)))
-(printf "This synthesized function contains a prefix p st. p followed by any word up to length k will not have the desired behavior.\n\n")
-(print-forms binding)
+;(define ww (symbolic-word* 3 '(0 1)))
+;(define binding
+;  (synthesize #:forall (list ww)
+;              #:guarantee (prefix-check S2 T2 prefixer ww)))
+;(printf "This synthesized function contains a prefix p st. p followed by any word up to length k will not have the desired behavior.\n\n")
+;(print-forms binding)
 
 ;; split states
 ; some state s in S where strings that arrive in s are both accepted and rejected by T
@@ -102,4 +106,9 @@
 
 (printf "\nSplit state hint:\n")
 
-(printf "\nWords that arrive in the state ~a have different behaviors on the true solution.\n\n" (solve-split-state S3 T3 (list 0 1) 3))
+;(printf "\nWords that arrive in the state ~a have different behaviors on the true solution.\n\n" (solve-split-state S3 T3 (list 0 1) 3))
+(define split-state-words (exists-word-exists-word S3 T3 (list 0 1) 10 split-state-pred))
+
+(if (empty? split-state-words)
+    (printf "\nNo split state was found when checking words up to ~a in length.\n\n" 10)
+    (printf "\nWords that arrive in the state ~a have different behaviors on the true solution.\n\n" (S3 (word-value (car split-state-words)))))
